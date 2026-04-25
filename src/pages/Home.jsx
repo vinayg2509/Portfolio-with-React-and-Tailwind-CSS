e1b3770ed2b13943f8aecf5cddd7c84693eeb028
vinayg2509 <vinayg2509@gmail.com>
2026-04-25 11:39:21 +0530

    [Vinay]initialize portfolio home layout with theme, animations, and utility styles

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Toggle Theme */}
+
+      {/* BackGround Effect */}
+
+      {/* Navbar */}
+
+      {/* Main Content */}
+
+
+      {/* Footer */}
+    </div>
+  )
+}
